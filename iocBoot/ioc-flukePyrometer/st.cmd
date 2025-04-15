6c8b93f3fa09d971f5a1f1bdb853741aa227e3b0
Bruno Martins <brunoseivam@gmail.com>
2025-04-14 22:44:04 -0400

    First batch of commands

--- iocBoot/ioc-flukePyrometer/st.cmd
+++ iocBoot/ioc-flukePyrometer/st.cmd
@@ -2,10 +2,18 @@
 
 < envPaths
 
+epicsEnvSet("STREAM_PROTOCOL_PATH", ".:$(TOP)/proto")
+epicsEnvSet("PREFIX", "PYRO:")
+epicsEnvSet("PORT", "P0")
+
 dbLoadDatabase("$(TOP)/dbd/flukePyrometer.dbd")
 flukePyrometer_registerRecordDeviceDriver(pdbbase)
 
-## Load record instances
-#dbLoadRecords("$(TOP)/db/flukePyrometer.db","user=bmartins")
+drvAsynIPPortConfigure("$(PORT)", "localhost:6363")
+asynOctetSetInputEos("$(PORT)",0,"\r\n")
+asynOctetSetOutputEos("$(PORT)",0,"\r\n")
+
+dbLoadRecords("$(TOP)/db/flukePyrometer.db","P=$(PREFIX),R=,PORT=$(PORT)")
+dbLoadRecords("$(ASYN)/db/asynRecord.db","P=$(PREFIX),R=asyn,PORT=$(PORT),ADDR=0,OMAX=32,IMAX=32")
 
 iocInit()
