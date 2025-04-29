989d27c24e3159821e4eb3092e741dbc2ccf4176
Bruno Martins <brunoseivam@gmail.com>
2025-04-28 20:44:49 -0400

    Add autosave

--- iocBoot/ioc-pyro01/st.cmd
+++ iocBoot/ioc-pyro01/st.cmd
@@ -6,6 +6,8 @@ epicsEnvSet("STREAM_PROTOCOL_PATH", ".:$(TOP)/proto")
 epicsEnvSet("PREFIX", "PYRO:")
 epicsEnvSet("PORT", "P0")
 
+epicsEnvSet("AS_PATH", "$(TOP)/iocBoot/$(IOC)/autosave")
+
 dbLoadDatabase("$(TOP)/dbd/flukePyrometer.dbd")
 flukePyrometer_registerRecordDeviceDriver(pdbbase)
 
@@ -16,4 +18,11 @@ asynOctetSetOutputEos("$(PORT)",0,"\r\n")
 dbLoadRecords("$(TOP)/db/flukePyrometer.db","P=$(PREFIX),R=,PORT=$(PORT)")
 dbLoadRecords("$(ASYN)/db/asynRecord.db","P=$(PREFIX),R=asyn,PORT=$(PORT),ADDR=0,OMAX=32,IMAX=32")
 
+set_requestfile_path("$(AS_PATH)")
+set_savefile_path("$(AS_PATH)")
+
 iocInit()
+
+save_restoreSet_Debug(0)
+makeAutosaveFileFromDbInfo("$(AS_PATH)/info_settings.req", "autosaveFields")
+create_monitor_set("info_settings.req", 30, "")
